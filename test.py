e882a9ad5432fbb1f58bdd4450d48c7da3451ab4
martinsangdo <dothanhsang1984@yahoo.com>
2026-03-31 10:27:34 +0700

    - Add some sample functions

---
+++ test.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def calculate_age(birth_year):
+    # Get the current year from the system clock
+    current_year = datetime.now().year
+    
+    # Calculate the difference
+    age = current_year - birth_year
+    
+    return age
+
+my_age = calculate_age(2000)
+print(my_age)
